cfdfa0a59769b644b7c1edb964c6f4c4477d5d26
alexander.p.conrad <alexander.p.conrad@3ee70800-55ec-11de-9c53-f1504392b436>
2009-07-06 22:22:41 +0000

    resetting repository, removing temporary files

--- trunk/tests/misc/test.R
+++
@@ -1,8 +0,0 @@
-source("AnalyzePrioritizations.R")
-
-Dataset=SubsetMutationOperator(SubsetMutationRate(SubsetChildDensity(SubsetFitnessTransform(SubsetPopSize(RealData,50),"EXP"),0.6),0.1),"ISM")
-
-MakeLatticePlotSelectionMethodRS(Dataset, 
-    title="Selection Operators: CE Focused Real-World Data Set", 
-    filesuffix="SELOP_MUTOP=ISM_MUTRT=0.1_CHLDR=1.0_FITOP=WIN_POPSZ=150",
-    datatype=1, color=FALSE)
